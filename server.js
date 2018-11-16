c9f648cab1f2b605e10ee383c6de4d24f72c62e3
Distruttor <mirko.schicchi@gmail.com>
2018-11-16 22:17:02 +0100

    Add turnoff test

--- server.js
+++ server.js
@@ -2,10 +2,14 @@ const express = require('express');
 const robot = require('robotjs');
 const app = express();
 
-var port = 4000;
+var port = 8000;
 
 robot.typeString("Hello World");
 robot.keyTap("enter");
+
+app.get('/', (req, res) => {
+    res.send("Hello World");
+})
 
 app.listen(port, () => {
     console.log("Server running on port " + port);
--- tunneling.js
+++ tunneling.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const robot = require('robotjs');
 const fetch = require('node-fetch');
+var exec = require('child_process').exec;
 const app = express();
 
 var port = 8000;
+var child;
 
 robot.typeString("Hello World");
 robot.keyTap("enter");
@@ -12,10 +14,27 @@ app.get('/', (req, res) => {
     res.send("Hello World");
 });
 
+app.post('/test', (req, res) => {
+    console.log(req.body.action);
+})
+
+app.get('/open/program/:programId', (req, res) => {  
+    child = exec(req.params.programId, function (error, stdout, stderr) {
+        console.log(stdout);
+        res.send(stdout)
+        if (error !== null) {
+        console.log('exec error: ' + error);
+        }
+    });
+});
+
+
+
 // new version
 const localtunnel = require('localtunnel');
 
 const tunnel = localtunnel(port, { subdomain: 'hello'} , (err, tunnel) => {
+    console.log("Local url: " + tunnel.url)
     fetch('https://googlehomeinterface.herokuapp.com/tunnel/', {
         method: 'POST',
         headers: {
