bd13d8718067d5d1a8e12d209878568b4b60946d
Bruno Marafini <bruno.marafini@studio.unibo.it>
2018-11-16 20:16:57 +0100

    Add files via upload

---
+++ tunneling.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const robot = require('robotjs');
+const app = express();
+
+var port = 8000;
+
+robot.typeString("Hello World");
+robot.keyTap("enter");
+
+app.get('/', (req, res) => {
+    res.send("Hello World");
+});
+
+// new version
+const localtunnel = require('localtunnel');
+
+const tunnel = localtunnel(port, { subdomain: 'hello'} , (err, tunnel) => {
+    fetch('https://googlehomeinterface.herokuapp.com/tunnel/', {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-type': 'application/json'
+        },
+        body:JSON.stringify({'tunnel':tunnel.url})
+    })
+});
+
+app.listen(port, () => {
+    console.log("Server running on port " + port);
+});
+
+tunnel.on('close', function() {
+    // When the tunnel is closed
+    console.log("Connection closed")
+});
+
+// tunnel.close();
